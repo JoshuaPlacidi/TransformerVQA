a837a536480343ae169414a0d2fb35905f0f67cf
JoshuaPlacidi <JoshuaPlacidi@gmail.com>
2022-02-20 22:28:14 +0000

    initial file creation

---
+++ config.py
@@ -0,0 +1,1 @@
+# file to store configuration details of model
---
+++ run.py
@@ -0,0 +1,5 @@
+import torch
+
+from training import train, evaluate
+from model import get_model
+from dataset import get_tgif_dataset
