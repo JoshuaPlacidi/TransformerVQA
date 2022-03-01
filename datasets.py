f88a463effa4a3d338c56ac1f69d6b1f8514b057
JoshuaPlacidi <JoshuaPlacidi@gmail.com>
2022-03-01 07:57:45 +0000

    datasets init

--- datasets.py
+++ datasets.py
@@ -1,3 +1,23 @@
 import torch
+from torch.utils.data import Dataset
 
+class TGIF_dataset():
+	def __init__(self, image_folder, annotation_file, mode="train"):
+		self.data = [1]
 
+	def __len__(self):
+		return len(self.data)
+
+	def __getitem__(self, idx):
+		return self.data[idx]
+
+def get_dataset(data_source="TGIF", image_folder=None, annotation_file=None):
+	if not (image_folder and annotation_file):
+		raise Exception("Both image_folder and annotation_file location are required, 1 or both not passed")
+
+	modes = ["train", "val", "test"]
+
+	if data_source=="TGIF":
+		return [TGIF_dataset(image_folder, annotation_file, mode) for mode in modes]
+	else:
+		raise Exception("data source not recognised:", data_source)		
--- run.py
+++ run.py
@@ -4,7 +4,10 @@ import config
 
 #from training import train, evaluate
 from model import get_PTVQA
-#from dataset import get_tgif_dataset
+from datasets import get_dataset
+
+train_dataset, val_dataset, test_dataset = get_dataset("TGIF", "/data/tgif/images", "data/tgif/annotation_file.txt")
 
 model = get_PTVQA()
 print(model)
+print('number of params:', sum(p.numel() for p in model.parameters() if p.requires_grad))
