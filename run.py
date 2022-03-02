030f7262196da23a29c8b9c90ff1d896eb7490ca
Andres <andresmenagodino@gmail.com>
2022-03-02 16:41:31 +0000

    Dataloader GIF_preproc

--- run.py
+++ run.py
@@ -7,7 +7,7 @@ from model import get_PTVQA
 from datasets import get_dataset
 
 train_set, val_set, test_set = get_dataset(
-    data_source = "TGIF",
+    data_source = "GIF_preproc", # TGIF
     image_folder = config.tgif_folder_location + "gifs/",
     annotation_file = config.tgif_folder_location + "train_action_question.csv"
     )
