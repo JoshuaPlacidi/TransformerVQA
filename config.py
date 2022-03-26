Fixing one incorrect shape

config.py
@@ -1,7 +1,7 @@
 # file to store configuration details of model
 import torch
 
-train_bert = True
+train_bert = False
 train_deit = False
 apply_masking_attn = False
 percentage_validation = 0.25 # Use only the 1/4 of the validation set
@@ -10,7 +10,7 @@ learning_rate = 0.00005
 feature_extractor = "deit"
 encoder_num_layers = 6
 batch_size = 50
-
+num_answers = 18
 check_point_name = f"train_bert_{str(train_bert)}_train_deit_{train_deit}_attn_mask_{str(apply_masking_attn)}_lr_{learning_rate}_fe_{feature_extractor}_batch_{batch_size}"
 
 # Specify a different checkpoint to load pre-trained model. Only if resume_checkpoint == True
@@ -19,18 +19,18 @@ check_point_load = "train_bert_True_attn_mask_False_lr_5e-05_2"
 
 print(f"\n\nCURRENT EXECUTION: {check_point_name}\n\n. From checkpoint: {resume_checkpoint}")
 
-use_gpu = True
+use_gpu = False
 print("Initialization in cofig.py:")
 print(f"Cuda available: {torch.cuda.is_available()}")
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 # device = torch.cuda.current_device()
-# device = "cpu"
+device = "cpu"
 
 number_devices = torch.cuda.device_count()
 print(f"Cuda current_device: {device}")
 print(f"Cuda device_count: {number_devices}")
-batch_size = batch_size * number_devices
+batch_size = max(batch_size, batch_size * number_devices)
 
 image_size = (224,224)
 
@@ -49,6 +49,5 @@ padded_language_length_answer = 6
 #
 tgif_folder_location = "F:/dev/datasets/tgif/"
 tgif_folder_location = "/home/mena/Documents/Master/mlp_sem2/PTVQA/data/tgif/"
-iqa_folder_location = "./data/iqa/"
 coco_folder_location = "./data/iqa/"
 vqa_annotation_file_location = "./data/iqa/annotations_coco.csv"

model.py
@@ -124,7 +124,7 @@ class IQA(nn.Module):
 		# iqa[0][1] == reshaped_iqa[1] All true. Makes sense
 		reshaped_iqa = torch.reshape(iqa, shape=(i_new.shape[0] * a.shape[1], -1, config.h_dim))
 
-		image_mask = torch.full(size=(i.shape[0]*5, 1), fill_value =1).to(i.device)
+		image_mask = torch.full(size=(i.shape[0]*a.shape[1], 1), fill_value =1).to(i.device)
 		whole_mask = torch.cat([image_mask, q_mask_reshaped, stacked_a_masks], axis=1)
 		#  We need to convert the mask to boolean, and invert it, because: 
 		# True value indicates that the corresponding key value will be ignored for the purpose of attention